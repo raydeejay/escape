de27ce32b7724c0da5023e7f1a2f601129ce867e
Sergi Reyner <sergi.reyner@gmail.com>
2017-03-31 01:11:36 +0100

    Removed code, renamed functions, resized window
    And possibly some other changes

--- display.lisp
+++ display.lisp
@@ -3,12 +3,10 @@
 (in-package #:escape)
 
 (defparameter *display-window* nil)
+(defparameter *display-fullscreen* nil)
 (defparameter *display-thread* nil)
-(defparameter *display-width* 640)
-(defparameter *display-height* 400)
-
-(defparameter *paper* (list 0 0 0 1)
-  "RGBA color of the background of the drawing field.")
+(defparameter *display-width* 800)
+(defparameter *display-height* 480)
 
 ;; accounting for the inverted Y axis and operating on the fourth quadrant
 ;; not quite sure how... other than by swapping SIN and COS in the usual formula
@@ -30,49 +28,53 @@
   (gl:viewport 0 0 width height)
   (gl:matrix-mode :projection)
   (gl:load-identity)
-  (gl:ortho 0 width height 0 -1 1)
+  (gl:ortho 0 *display-width* *display-height* 0 -1 1)
   (gl:matrix-mode :modelview))
 
+(defun toggle-fullscreen ()
+  (if *display-fullscreen*
+      (progn (sdl2:set-window-fullscreen *display-window* nil)
+             (gl-setup *display-width* *display-height*))
+      (progn (sdl2:set-window-fullscreen *display-window* :desktop)
+             (multiple-value-bind (w h)
+                 (sdl2:get-window-size *display-window*)
+               (gl-setup w h)))))
 
 (defun idle-func (win)
   (declare (ignore win))
-  ;; redraw lines
-  (loop :for sprite :in *sprites*
-     :initially (progn (gl:load-identity)
-                       (gl-setup *display-width* *display-height*)
-                       ;; clear the display
-                       (destructuring-bind (r g b a)
-                           *paper*
-                         (gl:clear-color r g b a))
-                       (gl:clear :color-buffer :depth-buffer))
-
-     :finally (progn (draw-sprites))))
+  ;; clear the display
+  (gl:clear-color 0 0 0 1)
+  (gl:clear :color-buffer)
+  ;; draw things
+  (draw-sprites))
 
 
 (defun handle-keydown (keysym)
   (let ((scancode (sdl2:scancode-value keysym))
-        ;; (sym (sdl2:sym-value keysym))
-        ;; (mod-value (sdl2:mod-value keysym))
-        )
-    (cond
-      ((sdl2:scancode= scancode :scancode-w)
-       (forward 10))
-      ((sdl2:scancode= scancode :scancode-s)
-       (back 10))
-      ((sdl2:scancode= scancode :scancode-a)
-       (left 15))
-      ((sdl2:scancode= scancode :scancode-d)
-       (right 15)))
-    ;; (format t "Key sym: ~a, code: ~a, mod: ~a~%"
-    ;;         sym
-    ;;         scancode
-    ;;         mod-value)
+        (sym (sdl2:sym-value keysym))
+        (mod-value (sdl2:mod-value keysym)))
+    (declare (ignore scancode sym mod-value))
+    ;; (cond
+    ;;   ((sdl2:scancode= scancode :scancode-r)
+    ;;    (make-scene)))
     ))
 
 (defun handle-keyup (keysym)
-  (when (sdl2:scancode= (sdl2:scancode-value keysym)
-                        :scancode-escape)
-    (sdl2:push-event :quit)))
+  (let ((scancode (sdl2:scancode-value keysym))
+        (sym (sdl2:sym-value keysym))
+        (mod-value (sdl2:mod-value keysym)))
+    (declare (ignore sym mod-value))
+    (cond ((sdl2:scancode= scancode :scancode-f11)
+           (toggle-fullscreen)
+           (setf *display-fullscreen* (not *display-fullscreen*)))
+
+          ((sdl2:scancode= scancode :scancode-r)
+           (make-scene))
+
+          ((sdl2:scancode= (sdl2:scancode-value keysym)
+                           :scancode-escape)
+           (sdl2:push-event :quit)))))
+
 
 (defun handle-mouse (button)
   (declare (ignore button))
@@ -87,31 +89,31 @@
                                       :rgba :unsigned-byte)
                       0))))
       (when (not (minusp c))
-        (let ((*sprite* (nth c *sprites*)))
+        (let ((sprite (nth c (sprites *scene*))))
           ;; action here
-          (forward 40)))))
+          (when sprite
+            (when (on-click sprite)
+              (funcall (on-click sprite))))))))
   (gl:scissor 0 0 *display-width* *display-height*)
   (gl:disable :scissor-test))
 
-(defun open-display% ()
+
+(defun open-display ()
   (sdl2:with-init
     (:everything)
-    (sdl2:with-window (win :title "Minilang Display"
+    (sdl2:with-window (win :title "Escape Game"
                            :w *display-width* :h *display-height*
                            :flags '(:shown :opengl))
       (setf *display-window* win)
       (sdl2:with-gl-context (gl-context win)
-        (setf *sprite* (make-instance 'sprite :filename "fire.png"))
-        (setf *sprites* (list *sprite*))
-
         (sdl2:gl-make-current win gl-context)
+
         (gl-setup *display-width* *display-height*)
-        (destructuring-bind (r g b a)
-            *paper*
-          (gl:clear-color r g b a))
-        (gl:clear :color-buffer :depth-buffer)
-        (load-textures)
+        (gl:clear-color 0 0 0 1)
+        (gl:clear :color-buffer)
         
+        (make-scene)
+
         (sdl2:with-event-loop (:method :poll)
           (:quit () t)
 
@@ -131,18 +133,15 @@
            ()
            (idle-func win)
            (sdl2:gl-swap-window win)
-           (sleep 0.001)))))))          ; naively 100fps?
+           (sleep 0.01)))))))          ; naively 100fps?
 
-(defun open-display ()
+(defun open-display-threaded ()
   (setf *display-thread*
         (make-thread 'open-display%
-                     :name "Minilang Display")))
+                     :name "Escape Game Display")))
 
 (defun close-display ()
   (sdl2:push-quit-event))
-
-(defun paper (r g b &optional (a 1.0))
-  (setf *paper* (list r g b a)))
 
 (defun save-png (filename)
   (il:with-init
