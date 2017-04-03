2987cc539007f16b8a1c9d9042ed02679b51d50b
Sergi Reyner <sergi.reyner@gmail.com>
2017-04-03 01:40:34 +0100

    Replaced LEFT-TO and RIGHT-TO macros with parsing
    The parsing was accidentally added in the last commit :D

--- escape.lisp
+++ escape.lisp
@@ -19,22 +19,15 @@
 ;;                          (make-sprite :knife "Knife" "images/knife.png" :x 100 :y 400
 ;;                                       :on-click [(setf *scene* (spawn (room02)))])))])))
 
-(eval-when (:compile-toplevel :load-toplevel :execute)
-  (defmacro left-to (room)
-    `(list :left "left" "images/arrowLeft.png" :x 10 :y 240
-           :on-click [(switch-to-scene (,room))])))
-
-(eval-when (:compile-toplevel :load-toplevel :execute)
-  (defmacro right-to (room)
-    `(list :right "right" "images/arrowRight.png" :x 630 :y 240
-           :on-click [(switch-to-scene (,room))])))
-
+(defun pickup (id)
+  "Returns a function that will pick up the object."
+  [(add *inventory* (discard *scene* id))])
 
 (defun room01 ()
   `(:room01 "images/room01.png"
-            ,(right-to room02)
+            (:right room02)
             (:box "Box" "images/box.png" :x 560 :y 410
-                  :on-click ,[(add *inventory* (discard *scene* :box))] )
+                  :on-click ,(pickup :box))
             (:portrait "Portrait" "images/portrait.png" :x 80 :y 150
                        :on-click ,[(when (selectedp :knife)
                                      (add *scene*
@@ -45,9 +38,7 @@
                                           (make-sprite :lighter "Lighter"
                                                        "images/lighter.png"
                                                        :x 130 :y 400
-                                                       :on-click
-                                                       [(add *inventory*
-                                                             (discard *scene* :lighter))]))
+                                                       :on-click (pickup :lighter)))
                                      (discard *inventory* :knife)
                                      (discard *scene* :portrait))])
             (:door "Door" "images/door.png" :x 300 :y 143
@@ -56,22 +47,21 @@
 
 (defun room02 ()
   `(:room02 "images/room02.png"
-            ,(left-to room01)
-            ,(right-to room03)
+            ;; (:left room01)
+            (:left room01)
+            (:right room03)
             (:vase "Vase" "images/vase.png" :x 560 :y 410
                    :on-click ,[ (when (selectedp :hammer)
                                   (add *scene*
                                        (make-sprite :knife "Knife" "images/knife.png"
                                                     :x 560 :y 410
-                                                    :on-click
-                                                    [ (add *inventory*
-                                                           (discard *scene* :knife)) ]))
+                                                    :on-click (pickup :knife)))
                                   (discard *scene* :vase))])))
 
 (defun room03 ()
   `(:room03 "images/room03.png"
-            ,(left-to room02)
-            ,(right-to room04)
+            (:left room02)
+            (:right room04)
             (:fireplace "Fireplace" "images/fireplace.png" :x 220 :y 144
                         :on-click
                         ,[(when (selectedp :logs)
@@ -88,31 +78,26 @@
                                  (add *scene*
                                       (make-sprite :key "Key" "images/key.png"
                                                    :x 350 :y 338
-                                                   :on-click
-                                                   [(add *inventory* (discard *scene* :key))]))
+                                                   :on-click (pickup :key)))
                                  (discard *inventory* :box))])
             (:axe "Axe" "images/axe.png" :x 360 :y 180
-                  :on-click ,[(add *inventory* (discard *scene* :axe))])
+                  :on-click ,(pickup :axe))
             (:toolbox "Toolbox" "images/toolbox.png" :x 120 :y 360
                       :on-click ,(once [(add *scene*
                                              (make-sprite :hammer "Hammer" "images/hammer.png"
                                                           :x 150 :y 420
-                                                          :on-click
-                                                          [ (add *inventory*
-                                                                 (discard *scene* :hammer))]))]))))
+                                                          :on-click (pickup :hammer)))]))))
 
 (defun room04 ()
   `(:room04 "images/room04.png"
-            ,(left-to room03)
+            (:left room03)
             (:tree "Tree" "images/tree.png" :x 470 :y 100
                    :on-click ,[(when (selectedp :axe)
                                  (add *scene*
                                       (make-sprite :logs "Logs"
                                                    "images/logs.png"
                                                    :x 570 :y 350
-                                                   :on-click
-                                                   [(add *inventory*
-                                                         (discard *scene* :logs))]))
+                                                   :on-click (pickup :logs)))
                                  (discard *inventory* :axe)
                                  (discard *scene* :tree))])))
 
